009ff6818327591343699ae18bee948b0c22ddf5
Cody Walters <jay_walters@discovery.com>
2020-03-07 11:53:26 -0800

    initial front-end implementation

---
+++ src/houseRouter/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      origin: '',
+      destinations: [],
+      optimizedList: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value
+    });
+  }
+
+  async handleSubmit() {
+    const { data } = await axios.post('http://localhost:3000/house-router-dev/v1', {
+      "origin": this.state.origin,
+      "destinations": this.state.destinations.split(';'),
+    });
+    this.setState({ optimizedList: data })
+    console.log('!! result =>  ', data, '!!');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>House Router</h1>
+          <p>What is start address?</p>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            name="origin"
+            defaultValue="13905 56th Pl S, Tukwila, WA 98168, USA"
+          />
+          <p>Add your list of addresses</p>
+          <textarea
+            rows="20"
+            cols="35"
+            name="destinations"
+            onChange={this.handleChange}
+            defaultValue="15146 65th Ave S, Tukwila, WA 98188, USA;15146 65th Ave S, Tukwila, WA 98188, USA;15210 Macadam Rd S APT D103, Tukwila, WA 98188, USA"
+          />
+          <input type="submit" value="Optimize Routes!" size="50" onClick={this.handleSubmit}></input>
+          <p></p>
+          <textarea
+            rows="10"
+            cols="50"
+            name="result"
+            readOnly={true}
+            value={this.state.optimizedList}
+          />
+          {/* <div>{this.state.optimizedList}</div> */}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/houseRouter/src/fixtures/destinations.js
@@ -0,0 +1,7 @@
+["13905 56th Pl S",
+"15146 65th Ave S",
+"15146 65th Ave S #503",
+"15210 Macadam Rd S D103",
+"15156 65th Ave S #1007",
+"14921 58th Ave S",
+"14218 53rd Ave S"]
